2a60cac9ba0923609798e0dc69790542cbcb19e6
tubagollu-lang <tubagollu@gmail.com>
2026-02-04 09:03:40 +0300

    Enhance data analysis and export in beginning_1.py
    Added data analysis and export functionality to the script.

---
+++ beginning_1.py
@@ -0,0 +1,34 @@
+import pandas as pd
+data = {
+    'Zaman': ['10:00', '10:05', '10:10', '10:15', '10:20'],
+    'Sicaklik_C': [22.5, 24.0, 23.5, None, 25.0],  
+    'Basinc_Pa': [1013, 1010, 1012, 1011, 1009],
+    'Durum': ['Normal', 'Normal', 'Normal', 'Hata', 'Yüksek']}
+df = pd.DataFrame(date)
+print("--- TÜM TABLO ---")
+print(df)
+#---------------------------------
+# 1. Sadece sütun isimlerini görelim
+print(df.columns)
+
+# 2. İstatistiksel Özet (Ortalama, min, max değerleri şak diye verir)
+print(df.describe())
+
+# 3. FİLTRELEME: Sadece Sıcaklığın 23'ten büyük olduğu anları getir
+yuksek_sicaklik = df[df['Sicaklik_C'] > 23]
+print("\n--- 23 Dereceden Sıcak Anlar ---")
+print(yuksek_sicaklik)
+
+# 4. Eksik Veriyi Yönetme (NaN olan satırı atalım)
+temiz_df = df.dropna()
+print("\n--- Eksik Verisi Olmayan Tablo ---")
+print(temiz_df)
+#-----------------------------------
+#Veriyi Excel dosyası olarak kaydediyoruz (Export)
+# 'index=False' diyoruz ki Pandas'ın sol tarafa koyduğu 0,1,2 numaralarını Excel'e yazmasın.
+df.to_excel('liste.xlsx', index=False)
+
+#Dosyayı sanki maille gelmiş gibi geri okuyoruz (Import)
+# Burada 'read_excel' komutu devreye giriyor
+df = pd.read_excel('liste.xlsx')
+
